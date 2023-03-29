024c04e32587d0c03d4c53298b232a3311d7330d
SpideyGio10 <104469485+SpideyGio10@users.noreply.github.com>
2023-03-28 18:55:27 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+video="";
+
+function setup(){
+    canvas=createCanvas(350,350);
+    canvas.center();
+    video=createCapture();
+    video.hide();
+}
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Estado: detectando objetos";
+    
+}
+function modelLoaded(){
+    console.log("¡Modelo cargado!");
+    status=true;
+}
+function draw(){
+    image(video,0,0,350,350);
+}
